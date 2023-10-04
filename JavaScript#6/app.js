bb6ea131823b41e8e010b407daf31f3708ee6301
mrsaadaarbi <145856837+mrsaadaarbi@users.noreply.github.com>
2023-10-04 09:19:29 -0700

    Add files via upload

---
+++ JavaScript#6/app.js
@@ -0,0 +1,83 @@
+// Chapter No:35-38
+
+//Q1 
+
+// var currentDate=new Date()
+// console.log(currentDate)
+
+// Q2
+
+// var firstName=prompt("Enter your first name")
+// var lastName=prompt("Enter your last name")
+// var fullName=firstName+" "+lastName
+// console.log("Welcome Mr-"+fullName)
+
+// Q3
+
+// var num1=parseInt(prompt("Enter Your First Value"));
+// var num2=parseInt(prompt("Enter Your Addition Value"));
+// var add=num1+num2
+// console.log(add)
+
+// Q4
+
+// function calc(num1,opr,num2){
+//     if(opr==="+"){
+//         return num1+num2
+//     }
+//     else if(opr==="*" ){
+//         return num1*num2
+
+//     }
+//     else if(opr==="-"){
+//         return num1-num2
+//     }
+//     else if(opr==="/"){
+//         return num1/num2
+//     }
+// }
+// var num1=+prompt("Enter your first number");
+// var opr=prompt("Enter your operator no");
+// var num2=+prompt("Enter your second number");
+// var result=calc(num1,opr,num2);
+// console.log(result)
+
+// Q5
+
+// function popup(){
+//     alert("Hello world")
+
+// }
+
+// popup()
+
+// Q7
+
+// function multiplicationTable(tableNumber, tableLength) {
+//       for (var i = 1; i <= tableLength; i++) {
+//         document.write(tableNumber + "x" + i + "=" + tableNumber * i + "<br>");
+//       }
+//     }
+    
+//     var tableNumber = +prompt("Enter a number to generate table...");
+//     var tableLength = +prompt("Enter table length..");
+    
+//     multiplicationTable(tableNumber, tableLength);
+
+ // Q10
+ 
+// var value=prompt("Enter a value for check")
+// var check=""
+// for(var i=value.length-1;i>=0;i--){
+//         console.log(value[i])
+//  check+=value[i]
+//     }
+//     if(value===check){
+//         console.log(value+ " is a palindrome word")
+//     }
+// else(
+//     console.log(value+ " is not a palindrome word")
+
+// )
+
+
